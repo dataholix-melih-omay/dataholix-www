1e2740df0005a04e174ba27c638654af1ce51cc1
Melih Omay <melih@omay.info>
2022-05-25 15:07:53 +0200

    [INIT] - New Projekt <no ticket>

---
+++ _posts/de/element-services/2022-05-23-data-analytics-Business-intelligence-lösungen.md
@@ -0,0 +1,7 @@
+---
+title: Data Analytics & Business Intelligence Lösungen
+element: services
+icon: bx bx-data
+---
+
+Deine Daten sind die Schätze von heute. Die Analyse, Extraktion, Transformation, Visualisierung un das Verständnis Deiner Daten ist Dein Kapital von morgen. Nutze Dein Datenpotential.
---
+++ assets/scripts/main.js
@@ -0,0 +1,35 @@
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+    const activeLink = 'active-link';
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 50,
+            sectionId = current.getAttribute('id'),
+            navClassList = document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList;
+
+        (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight)
+            ? navClassList.add(activeLink)
+            : navClassList.remove(activeLink);
+        }
+    );
+}
+// Einbinden
+window.addEventListener('scroll', scrollActive);
+
+
+/*=============== Ändere Hintergrund Header ===============*/
+function scrollHeader() {
+    const headerClassList = document.getElementById('header').classList;
+    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+
+    (this.scrollY >= 80)
+    ? headerClassList.add('scroll-header')
+    : headerClassList.remove('scroll-header');
+}
+
+// Einbinden
+window.addEventListener('scroll', scrollHeader);
