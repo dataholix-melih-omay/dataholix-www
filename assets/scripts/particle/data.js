0339447ef3c0fae75f79907be414b91c17dd08b1
Melih Omay <melih@omay.info>
2022-05-29 17:26:31 +0200

    [improved] - Disable mouse Events <no ticket> Partikel animation sind nicht mehr abhängig von der Mous und scrolling der webseite

--- assets/scripts/particle/data.js
+++ assets/scripts/particle/data.js
@@ -1,15 +1,21 @@
-import { swarmBackground } from 'https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js'
+// import { swarmBackground } from 'https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js'
+import { swarmBackground } from '/dataholix-www/assets/scripts/particle/threejs-toys.module.cdn.min.js';
 
 const bg = swarmBackground({
     el: document.getElementById('about'),
-    eventsEl: document.body.t,
+    eventsEl: undefined,
     gpgpuSize: 256,
     color: [Math.random() * 0xffffff, Math.random() * 0xffffff],
     geometry: 'default'
-})
+});
 
-bg.three.camera.position.set(0, 0, 200)
-
+bg.three.camera.position.set(0, 0, 200);
+// https://stackoverflow.com/questions/27410219/orbitcontrols-can-i-enable-disable-zooming-dynamicall
+// https://github.com/klevron/threejs-toys/
+// https://davidwalsh.name/css-variables-javascript
 document.body.addEventListener('click', () => {
-    bg.setColors([Math.random() * 0xffffff, Math.random() * 0xffffff])
-})
+    bg.setColors([
+        Math.random() * 0xffffff,
+        Math.random() * 0xffffff,
+    ]);
+});
