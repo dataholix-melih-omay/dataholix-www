d26d25e777eb527862b2b7e392b5ee53ae71fd0a
Melih Omay <melih.omay@dataholix.de>
2022-06-20 21:04:04 +0200

    [Bugfix] - Impressum und Datenschutz #1 Anpassung des Scriptes auf die neue Html Struktur wq

--- assets/scripts/policy-and-impress.js
+++ assets/scripts/policy-and-impress.js
@@ -1,32 +1,34 @@
 (function() {
     const policyId= "policy";
     const impressId= "impress";
+    const policyContentId = "policy-content";
+    const impressContentId = "impress-content";
+    
     const clickEvent = "click";
     const selectClass = "select"
     const visibilityClassName = "invisible";
-    
+
     const elementPolicy = document.getElementById(policyId);
     const elementImpress = document.getElementById(impressId);
     
-    const elementPolicyClassList = elementPolicy.nextElementSibling.classList;
-    const elementImpressClassList = elementImpress.nextElementSibling.classList;
-
-    function togglePolicy(e) {
-        elementPolicyClassList.toggle(visibilityClassName);
-        elementPolicy.classList.toggle(selectClass);
-        elementImpressClassList.add(visibilityClassName);
-        elementImpress.classList.remove(selectClass);
-
-    }
-
-    function toggleImpress(e) {
-        elementImpressClassList.toggle(visibilityClassName);
-        elementImpress.classList.toggle(selectClass);
-        elementPolicyClassList.add(visibilityClassName);
-        elementPolicy.classList.remove(selectClass);
-    }
+    const elementContentPolicy = document.getElementById(policyContentId);
+    const elementContentImpress = document.getElementById(impressContentId);
 
     elementPolicy.addEventListener(clickEvent, togglePolicy);
     elementImpress.addEventListener(clickEvent, toggleImpress);
 
+    function togglePolicy(e) {
+        elementPolicy.classList.toggle(selectClass);
+        elementImpress.classList.remove(selectClass);
+
+        elementContentPolicy.classList.toggle(visibilityClassName);
+        elementContentImpress.classList.add(visibilityClassName);
+    }
+    function toggleImpress(e) {
+        elementImpress.classList.toggle(selectClass);
+        elementPolicy.classList.remove(selectClass);
+
+        elementContentImpress.classList.toggle(visibilityClassName);
+        elementContentPolicy.classList.add(visibilityClassName);
+    }
 }());
