6af5a97c7f111ab608c5bba3d745bfe5e282aa03
Melih Omay <melih@omay.info>
2022-07-01 11:07:31 +0200

    [improved] - Openstreetmap <no ticket> Icon ertsellt und poistion gesetzt

--- assets/bundles/openstreet/init.js
+++ assets/bundles/openstreet/init.js
@@ -1,11 +1,25 @@
 (function () {
     function init() {
+        const latLonCoordinate = [11.54094, 48.12653 ];
+        const latLonCenter = ol.proj.fromLonLat(latLonCoordinate);
+        const view = new ol.View({
+            center: latLonCenter,
+            zoom: 18
+        });
+
+        const markers = new ol.layer.Vector({
+            title: 'dataholix solutions GmbH',
+            source: new ol.source.Vector(),
+            style: new ol.style.Style({
+                image: new ol.style.Icon({
+                    anchor: [0.5, 1],
+                    src: 'assets/bundles/openstreet/marker.png'
+                })
+            })
+        });
+
         const map = new ol.Map({
-            view: new ol.View({
-                // center: [0, 0],
-                center: ol.proj.fromLonLat([11.54094, 48.12653, ]),
-                zoom: 18
-            }),
+            view: view,
             layers: [
                 new ol.layer.Tile({
                     source: new ol.source.OSM
@@ -13,8 +27,11 @@
             ],
             target: "basic-map"
         });
+
+
+        map.addLayer(markers);
+        const marker = new ol.Feature(new ol.geom.Point(ol.proj.fromLonLat(latLonCoordinate)));
+        markers.getSource().addFeature(marker);
     }
     init();
 } () );
-
-// 481265393,115409431,
